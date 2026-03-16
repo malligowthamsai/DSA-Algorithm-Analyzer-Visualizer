d4ba64a6401f72bdd93cf0b12f225805db10892b
malligowthamsai <malligowthamsai@gmail.com>
2026-03-16 22:48:11 +0530

    Create BubbleSort.java

---
+++ src/main/java/com/github/dsa/algorithms/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.github.dsa.algorithm;
+
+public class BubbleSort<T extends Comparable<T>> implements SortAlgorithm<T> {
+
+    @Override
+    public void sort(T[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    // Swap
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getAlgorithmName() {
+        return "Bubble Sort (O(n^2))";
+    }
+}
